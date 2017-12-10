66c83f27685bb2f63be850a71051e6d8a9b0ede1
何永能 <yongneng.he@dianping.com>
2017-12-10 21:16:14 +0800

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,54 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['standard', 'eslint:recommended'],
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      impliedStrict: true,
+      experimentalObjectRestSpread: true
+    }
   },
-  env: {
-    browser: true
-  },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.wpy files
   plugins: [
     'html'
   ],
   settings: {
     'html/html-extensions': ['.html', '.wpy']
   },
-  // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
+  rules: {
+    camelcase: 2,
+    eqeqeq: 2,
+    curly: 2,
+    indent: [2, 2],
+    quotes: [2, 'single'],
+    semi: [2, 'always'],
+    'space-infix-ops': 2,
+    'comma-dangle': 1,
+    'no-undef': 1,
+    'global-strict': 0,
+    'no-extra-semi': 1,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': [1, {'skipBlankLines': true}],
+    'no-unreachable': 1,
+    'no-alert': 0,
+    'brace-style': [2, '1tbs'],
+    'block-scoped-var': 2,
     'generator-star-spacing': 0,
-    // allow debugger during development
+    'no-extra-parens': 2,
+    'no-cond-assign': [2, 'always'],
+    'linebreak-style': [2, 'unix'],
+    'eol-last': 2,
+    'padded-blocks': [2, 'never'],
+    'object-curly-spacing': [2, 'always', {objectsInObjects: false}],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
